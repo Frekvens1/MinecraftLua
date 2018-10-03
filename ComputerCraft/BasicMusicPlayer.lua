a072f4e64f61c8a9f51956c393a8f2b6562028fc
Frekvens1 <Frekvens1@gmail.com>
2018-10-03 21:37:18 +0200

    Update BasicMusicPlayer.lua

--- ComputerCraft/BasicMusicPlayer.lua
+++ ComputerCraft/BasicMusicPlayer.lua
@@ -2,7 +2,9 @@ local FileList = fs.list("./songs") -- Song folder
 
 while true do
   for _, file in ipairs(FileList) do
+    term.clear()
+    term.setCursoirPos(1, 1)
     print("Now playing: "..file)
-    shell.run("./songs/"...file)
+    shell.run("./songs/"..file)
   end
 end
