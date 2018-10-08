4b442170b9fabaa79f42398e3e92a7435c25c9d1
Frekvens1 <Frekvens1@gmail.com>
2018-10-08 21:08:36 +0200

    Create Album.lua

---
+++ ComputerCraft/Album.lua
@@ -0,0 +1,61 @@
+{
+	name = "Default songs",
+	creator = "Frekvens1",
+	songs = {
+		{
+			name = "Popcorn",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/Popcorn.lua"
+		},
+		{
+			name = "DJ Got Us Fallin' in Love",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/DJ.lua"
+		},
+		{
+			name = "Song of Storms",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/SongOfStorms.lua"
+		},
+		{
+			name = "He's A Pirate",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/HeIsAPirate.lua"
+		},
+		{
+			name = "Super Mario Bros 3 Athletic",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/Mario.lua"
+		},
+		{
+			name = "All Star",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/AllStar.lua"
+		},
+		{
+			name = "Fireflies",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/Fireflies.lua"
+		},
+		{
+			name = "Gangnam Style",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/GangnamStyle.lua"
+		},
+		{
+			name = "Friday",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/Friday.lua"
+		},
+		{
+			name = "Tetris",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/Tetris.lua"
+		},
+		{
+			name = "Don't Stop Me Now",
+			length = 1,
+			url = "http://127.0.0.1/CC/songs/DontStopMeNow.lua"
+		}
+	}
+}
