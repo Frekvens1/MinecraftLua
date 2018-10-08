838b4c84b4d2032e9a74e1823bb98186d64c9fcb
Frekvens1 <Frekvens1@gmail.com>
2018-10-08 21:22:06 +0200

    Update Album.lua

--- ComputerCraft/Album.lua
+++ ComputerCraft/Album.lua
@@ -5,57 +5,57 @@
 		{
 			name = "Popcorn",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/Popcorn.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/Popcorn.lua"
 		},
 		{
 			name = "DJ Got Us Fallin' in Love",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/DJ.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/DJ.lua"
 		},
 		{
 			name = "Song of Storms",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/SongOfStorms.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/SongOfStorms.lua"
 		},
 		{
 			name = "He's A Pirate",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/HeIsAPirate.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/HeIsAPirate.lua"
 		},
 		{
 			name = "Super Mario Bros 3 Athletic",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/Mario.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/SuperMarioBros3Athletic.lua"
 		},
 		{
 			name = "All Star",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/AllStar.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/AllStar.lua"
 		},
 		{
 			name = "Fireflies",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/Fireflies.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/Fireflies.lua"
 		},
 		{
 			name = "Gangnam Style",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/GangnamStyle.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/GangnamStyle.lua"
 		},
 		{
 			name = "Friday",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/Friday.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/Friday.lua"
 		},
 		{
 			name = "Tetris",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/Tetris.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/Tetris.lua"
 		},
 		{
 			name = "Don't Stop Me Now",
 			length = 1,
-			url = "http://127.0.0.1/CC/songs/DontStopMeNow.lua"
+			url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/ComputerCraft/Songs/DontStopMeNow.lua"
 		}
 	}
 }
