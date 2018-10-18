aaad3197d924ae774ba9b3251a3d494a21021bdc
Frekvens1 <Frekvens1@gmail.com>
2018-10-19 00:28:45 +0200

    Update tape.lua

--- Computronics/Golang/dfpwm/tape.lua
+++ Computronics/Golang/dfpwm/tape.lua
@@ -1,7 +1,10 @@
 tape = peripheral.find("tape_drive")
 
---local url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Tubthumping.dfpwm"
-local url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Electronic%20Super%20Joy%20OST%20-%2002%20Flare.dfpwm"
+local url = ""
+--url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Tubthumping.dfpwm"
+--url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Electronic%20Super%20Joy%20OST%20-%2002%20Flare.dfpwm"
+--url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Teminite%20%26%20Panda%20Eyes%20-%20Highscore.dfpwm"
+url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Kaivon%20-%20Reborn.dfpwm"
 
 local response = http.get(url, nil, true)
 local file = response.readAll()
