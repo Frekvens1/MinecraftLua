df4bdad55bf3102ed807ae011643f579e2e78356
Frekvens1 <Frekvens1@gmail.com>
2018-10-18 06:48:14 +0200

    Update tape.lua

--- Computronics/Golang/dfpwm/tape.lua
+++ Computronics/Golang/dfpwm/tape.lua
@@ -1,6 +1,8 @@
 tape = peripheral.find("tape_drive")
 
-local url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Tubthumping.dfpwm"
+--local url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Tubthumping.dfpwm"
+local url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Electronic%20Super%20Joy%20OST%20-%2002%20Flare.dfpwm"
+
 local response = http.get(url, nil, true)
 local file = response.readAll()
 response.close()
