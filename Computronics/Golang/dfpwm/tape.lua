b5bfe0733ea99a0088af7a3a01ce6e4006de8dee
Frekvens1 <Frekvens1@gmail.com>
2018-10-19 02:49:02 +0200

    Update tape.lua

--- Computronics/Golang/dfpwm/tape.lua
+++ Computronics/Golang/dfpwm/tape.lua
@@ -1,15 +1,49 @@
 tape = peripheral.find("tape_drive")
 
-local url = ""
---url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Tubthumping.dfpwm"
---url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Electronic%20Super%20Joy%20OST%20-%2002%20Flare.dfpwm"
---url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Teminite%20%26%20Panda%20Eyes%20-%20Highscore.dfpwm"
-url = "https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Kaivon%20-%20Reborn.dfpwm"
+function get(url)
+	local result = nil
+	
+	if not http,checkURL(url) then
+		return nil
+	end
+	
+	local response = http.get(url, nil, true)
+	if not response then
+		return nil
+	end
+	
+	result = response.readAll()
+	response.close()
+	
+	return result
+end
 
-local response = http.get(url, nil, true)
-local file = response.readAll()
-response.close()
 
-tape.seek(-tape.getPosition()) -- Rewind
-tape.write(file)
-tape.seek(-tape.getPosition()) -- Rewind
+local files = {
+	get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Tubthumping.dfpwm"),
+	get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Electronic%20Super%20Joy%20OST%20-%2002%20Flare.dfpwm"),
+	get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Teminite%20%26%20Panda%20Eyes%20-%20Highscore.dfpwm"),
+	get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/Kaivon%20-%20Reborn.dfpwm"),
+}
+
+
+while true do
+	
+	
+	for i=1, #files, 1 do
+		tape.stop()
+		tape.seek(-tape.getPosition()) -- Rewind
+		
+		tape.write(files[i])
+		
+		tape.seek(-tape.getPosition()) -- Rewind
+		tape.play()
+		
+		while not tape.getPosition() >= #files[i] do
+			os.sleep(1)
+		end
+		
+	
+	end
+	
+end
