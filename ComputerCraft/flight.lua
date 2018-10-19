17321b72b0b608f824ae405b02e3623b410ce221
Frekvens1 <Frekvens1@gmail.com>
2018-10-20 00:53:49 +0200

    Update flight.lua

--- ComputerCraft/flight.lua
+++ ComputerCraft/flight.lua
@@ -11,6 +11,29 @@ function readTable(tb)
 	end
 end
 
+hover = false
+
+function doHover()
+
+	while hover do
+		-- We calculate the required motion we need to take
+		local mY = meta.motionY
+		mY = (mY - 0.138) / 0.8
+
+		-- If it is sufficiently large then we fire ourselves in that direction.
+		if mY > 0.5 or mY < 0 then
+			local sign = 1
+			if mY < 0 then sign = -1 end
+				modules.launch(0, 90 * sign, math.min(4, math.abs(mY)))
+		else
+			sleep(0)
+		end
+	end
+	
+end
+
+end
+
 while true do
 	local event = {os.pullEvent()}
 	
@@ -23,9 +46,13 @@ while true do
 			
 			pressed = true
 		end
+	elseif event[1] == "key" and event[2] == 35 then
+		hover = true
 	end
 	
 	if event[1] == "key_up" and event[2] == key then
 		pressed = false
 	end
+	
+	doHover()
 end
