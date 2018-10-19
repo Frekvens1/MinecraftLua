7f313e805f94be7c2ffe73fc5fe222f373bb0a57
Frekvens1 <Frekvens1@gmail.com>
2018-10-19 23:05:48 +0200

    Update flight.lua

--- ComputerCraft/flight.lua
+++ ComputerCraft/flight.lua
@@ -2,12 +2,21 @@ local modules = peripheral.find("neuralInterface")
 
 meta = modules.getMetaOwner()
 
+local pressed = false
+
 while true do
 	local event = {os.pullEvent()}
 	
-	if event[1] == "key" and key == 76 then
-		meta = modules.getMetaOwner()
-		
-		modules.launch(meta.yaw, meta.pitch, 3)
+	if event[1] == "key" and event[2] == 76 then
+		if not pressed then
+			meta = modules.getMetaOwner()
+			modules.launch(meta.yaw, meta.pitch, 3)
+			
+			pressed = true
+		end
+	end
+	
+	if event[1] == "key_up" and key == 76 then
+		pressed = false
 	end
 end
