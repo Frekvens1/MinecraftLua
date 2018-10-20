e7774eb6cb8b861899fa4aaf9118e51bbfa19ef1
Frekvens1 <Frekvens1@gmail.com>
2018-10-20 14:36:36 +0200

    Update SkellyAimbot.lua

--- ComputerCraft/SkellyAimbot.lua
+++ ComputerCraft/SkellyAimbot.lua
@@ -1,50 +1,51 @@
 --    wget http://127.0.0.1/CC/SkellyAimbot.lua protec    --
 local master = "Frekvens1"
+
 interface = peripheral.find("neuralInterface")
 
+if not meta.name == "Skeleton" then error("Entity must be a skeleton!") end
 if not interface.hasModule("plethora:kinetic", 0) then error("Must have a kinetic agument", 0) end
 if not interface.hasModule("plethora:sensor") then error("Must have a sensor", 0) end
-	
+
+--[[ -- Super fast sleep --
 local function yield()
 	os.queueEvent( "sleep" )
 	coroutine.yield( "sleep" )
 end
-
-local meta = interface.getMetaOwner()
-
-if not meta.name == "Skeleton" then error("Entity must be a skeleton!") end
-interface.disableAI()
+--]]
 
 local mobs = {
-	Zombie = true,
-	Skeleton = true,
-	Creeper = true,
-	Spider = true,
-	Slime = true
+	Zombie = "Zombie",
+	Skeleton = "Skeleton",
+	Creeper = "Creeper",
+	Spider = "Spider",
+	CaveSpider = "Cave Spider",
+	Slime = "Slime"
 }
 
-local function filter(name)
-	if mobs[name] then
-		return true, name
+
+local function filter(entity)
+	if mobs[entity.name] then
+		if mobs[entity.name] == entity.displayName then
+			return true, entity.name
+		else
+			return false, "Friendly"
+		end
 	else
 		return false, "Player"
 	end
 end
 
-function clear()
-	term.clear()
-	term.setCursorPos(1,1)
-end
 
 function Buffer()
-
 	local self = {}
-	
 	self.bufferText = {}
 
+	
 	function self.add(text)
 		table.insert(self.bufferText, text)
 	end
+	
 	
 	function self.print()
 		term.clear()
@@ -52,15 +53,13 @@ function Buffer()
 			term.setCursorPos(1, k)
 			term.write(v)
 		end
-		
 		self.bufferText = {}
 	end
 	
 	return self
-	
 end
 
-local Angle = { }
+local Angle = {}
 function Angle.towards(entity)
 	
 	local x = entity.x + entity.motionX
@@ -73,61 +72,22 @@ function Angle.towards(entity)
 	return yaw, pitch
 end
 
-function Angle.predict(entity)
-	
-	local x = entity.x + (entity.motionX^2)*2
-	local y = entity.y + (entity.motionY^2)*2
-	local z = entity.z + (entity.motionZ^2)*2
-	
-	local yaw = math.deg(math.atan2(-x, z))
-	local pitch = -math.deg(math.atan2(entity.y, math.sqrt(entity.x^2 + entity.z^2)))
-	
-	return yaw, pitch
-end
-
+local meta = interface.getMetaOwner()
+interface.disableAI()
 buffer = Buffer()
 
-parallel.waitForAny(
-
-		function()
-			while true do
-				meta = interface.getMetaOwner()
-				yield()
-			end
-		end,
-		
-		
-		function()
-			while true do
+while true do
 			
-				event = {os.pullEvent()}
-				if event[1] == "chat_message" then
-					username = event[2]
-					message = event[3]
-					if (username == master) and (message == "fire") then
-						interface.shoot(1)
-					end
-				end
-				
-			end
-		end,
-		
-		function()
-			while true do
-			
-				buffer.print()
-				for k, entity in pairs(interface.sense()) do
-					if not (entity.id == meta.id) then
-						if filter(entity.name) and (entity.name == entity.displayName) then
-							buffer.add(entity.name)
+	buffer.print()
+	for k, entity in pairs(interface.sense()) do
+		if filter(entity.name) and (entity.name == entity.displayName) and not (entity.id == meta.id) then
+			buffer.add(entity.name)
 							
-							local yaw, pitch = Angle.predict(entity)
-							interface.look(yaw, pitch)
-							interface.fire(yaw, pitch, 5)
-						end
-					end
-				end
-				
-			end
+			local yaw, pitch = Angle.predict(entity)
+			interface.look(yaw, pitch)
+			interface.fire(yaw, pitch, 0.5) --0.5 Doesn't destroy blocks, and weaker.
+							
 		end
-)
+	end	
+	
+end
