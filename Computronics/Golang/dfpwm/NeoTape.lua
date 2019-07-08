c8458ba1285e4f3c150e73360b6505ee40acba3c
Frekvens1 <Frekvens1@gmail.com>
2019-07-08 16:31:37 +0200

    Create NeoTape.lua

---
+++ Computronics/Golang/dfpwm/NeoTape.lua
@@ -0,0 +1,41 @@
+tape = peripheral.find("tape_drive")
+
+function get(url)
+	local result = nil
+	
+	if not http.checkURL(url) then
+		return nil
+	end
+	
+	local response = http.get(url, nil, true)
+	if not response then
+		return nil
+	end
+	
+	result = response.readAll()
+	response.close()
+	
+	return result
+end
+
+
+local files = {
+	--get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/"),
+}
+
+
+while true do
+	for i=1, #files, 1 do
+		tape.stop()
+		tape.seek(-tape.getPosition()) -- Rewind
+		
+		tape.write(files[i])
+		
+		tape.seek(-tape.getPosition()) -- Rewind
+		tape.play()
+		
+		while not ((tape.getPosition() >= #files[i]) or tape.isEnd()) do
+			os.sleep(1)
+		end
+	end
+end
