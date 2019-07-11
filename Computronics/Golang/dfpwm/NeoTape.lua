b2f1d6921a561a85d584a1e66c752297536b46c8
Frekvens1 <Frekvens1@gmail.com>
2019-07-11 02:39:00 +0200

    Update NeoTape.lua

--- Computronics/Golang/dfpwm/NeoTape.lua
+++ Computronics/Golang/dfpwm/NeoTape.lua
@@ -21,20 +21,36 @@ end
 
 local files = {
 	--get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/"),
-	get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/RISE.dfpwm"),
-	get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/OliverTree_Alie%20Boy.dfpwm"),
+	{
+		get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/FallenKingdom.dfpwm"),
+		"Fallen Kingdom",
+	},
+	{
+		get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/RISE.dfpwm"),
+		"Rise - League of Legends",
+	},
+	{
+		get("https://raw.githubusercontent.com/Frekvens1/MinecraftLua/master/Computronics/Golang/dfpwm/OliverTree_Alie%20Boy.dfpwm"),
+		"Oliver Tree - All I Got",
+	},
 }
 
 for i=1, #files, 1 do
+
+	term.clear()
+	term.setCursorPos(1,1)
+	
+	print("Currently playing "..files[i][2])
+	
 	tape.stop()
 	tape.seek(-tape.getPosition()) -- Rewind
 		
-	tape.write(files[i])
+	tape.write(files[i][1])
 		
 	tape.seek(-tape.getPosition()) -- Rewind
 	tape.play()
 		
-	while not ((tape.getPosition() >= #files[i]) or tape.isEnd()) do
+	while not ((tape.getPosition() >= #files[i][1]) or tape.isEnd()) do
 		os.sleep(1)
 	end
 end
